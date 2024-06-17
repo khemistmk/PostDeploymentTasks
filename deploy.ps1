c43c1462db75023e570b854e448eb6ef4fcf151c
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-06-16 19:41:53 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,20 @@
 # Deployment Script
 # by khemist
- "---------------------------"
+#
+# ---------------------------
+#
+#This script will perform the following actions under the default profile:
+# * check and Set computer name to serial number
+# * check activation and activate windows based on OEM bios key
+# * install and run System Update if Lenovo
+# * run windows updates
+# * run Ninite to install Chrome, Firefox, and VLC
+# * activate bitlocker and print bitlocker key to file
+# * install .Net 3.5
+# * uninstall SmartDeploy
+# * remove Deployment folders
 
-$serial = wmic bios get serialnumber
-$cpuInfo = Get-CimInstance -ClassName Win32_Processor
-$cpuname = $cpuInfo.Name
 
-Function Get-OSName
-{
-    (Get-WmiObject Win32_OperatingSystem).Caption
-}
-$osversion = Get-OSName
 
 Function Infoheader {
   Write-Host "---------------------------"
@@ -18,3 +22,17 @@ Function Infoheader {
   Write-Host "     Deployment Script v0.1     "
   Write-Host "---------------------------" 
   }
+
+
+$serialnumber = wmic bios get serialnumber
+$product key = wmic path softwarelicensingservice get OA3OriginalProductKey
+
+$cpuInfo = Get-CimInstance -ClassName Win32_Processor
+
+$cpuname = $cpuInfo.Name
+
+Function Get-OSName
+{
+   $osversion = (Get-WmiObject Win32_OperatingSystem).Caption
+   write-output "$osversion"
+}
