9bc4ceace8555ce826643c82701783ce7bec6f65
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-06-16 19:55:05 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -50,3 +50,24 @@ Function windowsupdate {
     # Restart the system if updates require a reboot
     Restart-Computer -Force
 }
+
+function DotNet3 {
+    #Enables .Net 3.5
+    Write-Host "[*] Enabling .Net 3.5" -ForegroundColor Green
+    DISM /Online /Enable-Feature /FeatureName:NetFx3 /All
+    Write-Host "[*] .Net 3.5 Enabled"
+}
+
+function Bitlocker {
+    #Checks if Bitlocker enabled, if not, enables and prints recovery password to file
+    if (((Get-BitLockerVolume -MountPoint c:).VolumeStatus) -eq FullyEncrypted) {
+        Write-Host "[*] Bitlocker is already enabled for Drive C:"
+    }
+    else {
+        Enable-Bitlocker -MountPoint C -UsedSpaceOnly -RecoveryPassword
+    }
+    $Bitlockerkey = (Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object -Property KeyProtectorType -eq RecoveryPassword | Select-Object -Property KeyProtectorID,RecoveryPassword 
+    $Bitlockerkey > "$HOME\$computername.txt"
+    Write-Output "Bitlocker enabled. Bitlocker key is saved to $HOME\$computername.txt"
+    Write-Output "$Bitlockerkey"
+}
