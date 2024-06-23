d986a6ffad0be48c0f2cc7b232e0628feb5b8ead
khemistmk <twilson@threathorizon.org>
2024-06-22 21:36:43 -0500

    updated script

--- deploy.ps1
+++ deploy.ps1
@@ -18,22 +18,30 @@
 # * remove Deployment folders
 
 ####Update the following paths as needed####
+
+#script root folders
+$deployroot = "\\tcs-deploy\techshare\"
+$scriptroot = "C:\Setup Files"
+
 #System Update file location
-$systemupdate = "C:\Setup Files\System Update.exe"
+#grab latest from https://support.lenovo.com/us/en/downloads/ds012808-lenovo-system-update-for-windows-10-7-32-bit-64-bit-desktop-notebook-workstation
+$systemupdate = "System Update.exe"
 
 #HP Imaging Assistant file location
 #grab latest from http://ftp.ext.hp.com//pub/caps-softpaq/cmit/HPIA.html](http://ftp.ext.hp.com//pub/caps-softpaq/cmit/HPIA.html
-$HPIA = "C:\Setup Files\hp-hpia-5.2.1.exe"
+$HPIA = "hp-hpia-5.2.1.exe"
 
 #Ninite file location
-$ninite = "C:\Setup Files\AppInstaller32.exe"
-############################################
+$ninite = "AppInstaller32.exe"
 
+############################################
+clear
 #Structure
-Function Infoheader {
+Function Header {
+    clear
   Write-Host "---------------------------"
   Write-Host ""
-  Write-Host "     Post-Deployment Script v0.1     "
+  Write-Host "Post-Deployment Script v0.1"
   Write-Host ""
   Write-Host "---------------------------" 
 }
@@ -44,12 +52,15 @@ Function MainMenu {
     Write-Host ""
     Write-Host " (0) Default Deployment (No Office, No PDF)"
     Write-Host " (1) Custom Deployment"
+    Write-Host " (q) Quit"
     Write-Host ""
     Write-Host "---------------------------" 
     $Deploy = Read-Host "Please make a selection"
   }
 
-Function Default {
+Function InfoText {
+    clear
+    Header
     Write-Host "This script will perform the following actions under the default profile:"
     Write-Host "* Disable built in Administrator account"
     Write-Host "* check and Set computer name to serial number"
@@ -62,9 +73,12 @@ Function Default {
     Write-Host "* configure power plan: On Battery (screen off: 30min, sleep: never), Plugged in (screen off: 1 hr, sleep: never)"
     Write-Host "* uninstall SmartDeploy"
     Write-Host "* remove Deployment folders"
+    $Default = Read-Host " Press (1) to Deploy. Press (q) to quit"
 }
 
 function Custom {
+    clear
+    Header
     Write-Host "---------------------------"
     Write-Host ""
     Write-Host " Select Office Version"
@@ -74,6 +88,7 @@ function Custom {
     Write-Host "---------------------------" 
     $Office = Read-Host "Please make a selection"    
     clear
+    Header
     Write-Host "---------------------------"
     Write-Host ""
     Write-Host " Select PDF Version"
@@ -82,8 +97,38 @@ function Custom {
     Write-Host "    (2) Foxit Business"
     Write-Host ""
     Write-Host "---------------------------" 
-    $Office = Read-Host "Please make a selection"    
+    $PDF = Read-Host "Please make a selection"  
+    
+    if ($Office -eq '1') {
+        $officedeploy = "$deployroot\MS Office\Office 365\Setup32.exe"
+    }
+    
+}
+
+Function Customtext {
     clear
+    Header
+    Write-Host "This script will perform the following actions under the default profile:"
+    Write-Host "* Disable built in Administrator account"
+    Write-Host "* check and Set computer name to serial number"
+    Write-Host "* check activation and activate windows based on OEM bios key"
+    Write-Host "* install and run System Update if Lenovo, HP Image assistant if HP"
+    Write-Host "* run windows updates"
+    Write-Host "* run Ninite to install Chrome, Firefox, and VLC"
+    Write-Host "* activate bitlocker and print bitlocker key to file"
+    Write-Host "* install .Net 3.5"
+    Write-Host "* configure power plan: On Battery (screen off: 30min, sleep: never), Plugged in (screen off: 1 hr, sleep: never)"
+    Write-Host "* uninstall SmartDeploy"
+    Write-Host "* remove Deployment folders"
+    if ($Office -eq '1') {
+        Write-Host "* Install Microsoft 365 Apps"
+    }
+    else (Write-Host "No Office Apps Deployed")
+    if ($PDF -eq '1') {
+        Write-Host "* Install Adobe STD 2020"
+    }
+    else (Write-Host "No PDF Apps Deployed")
+    $Customdeploy = Read-Host " Press (1) to Deploy. Press (q) to quit"
 }
 
 #Installation and changes
@@ -97,7 +142,10 @@ Function ComputerName {
         Rename-Computer -newname "$computername"
     }
 }
-
+function DisableAdmin {
+    Start-Process -FilePath "$scriptroot\$disableadmin"
+    
+}
 Function WinActivation {
     $OEMproductkey = wmic path softwarelicensingservice get OA3OriginalProductKey
     $activationstatus = cscript c:\windows\system32\slmgr.vbs /xpr
@@ -141,23 +189,22 @@ function Bitlocker {
     Write-Output "Bitlocker enabled. Bitlocker key is saved to $HOME\$computername.txt"
     Write-Output "$Bitlockerkey"
 }
-
+ 
 Function SystemUpdate {
     $manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer 
     if ($Manufacturer -contains "Lenovo"){
-    Start-Process -FilePath $systemupdate -ArgumentList "/VERYSILENT /NORESTART"
-
+    Start-Process -FilePath "$scriptroot\$systemupdate" -ArgumentList "/VERYSILENT /NORESTART" -Wait
     Start-Process -FilePath "C:\Program Files (x86)\Lenovo\System Update\Tvsu.exe" -ArgumentList "/CM -search R -action INSTALL -nolicense -IncludeRebootPackages 1,3,4"
     }
     elseif ($manufacturer -contains "HP"){
-    Start-Process -Filepath $HPIA -ArgumentList "/s"
+    Start-Process -Filepath "$scriptroot\$HPIA" -ArgumentList "/s"
     Set-Location -FilePath "C:\SWSetup\SP140024"
     Start-Process -FilePath "HPImageAssistant.exe" -ArgumentList "/Action:Install /AutoCleanup /Category:BIOS, Drivers,Firmware /Silent"
     }
 }
 
 Function Ninite {
-    Start-Process -Filepath $Ninite
+    Start-Process -Filepath "$scriptroot\$Ninite"
 }
 
 Function RMDeployfiles {
@@ -174,20 +221,41 @@ Function SmartDeploy {
     Start-Process MsiExec.exe -ArgumentList "/X{77753FDC-5039-4F18-B37C-E86B7EF921A9}"
 }
 
-Infoheader
-MainMenu
-
-if ($deploy -eq '0'){
+Function Defaultdeploy {
     WinActivation
     ComputerName
     DotNet3
     Ninite
+    RMDeployfiles
+    SmartDeploy
     SystemUpdate
     windowsupdate
     Bitlocker
+}
+Infoheader
+MainMenu
+
+switch ($deploy){
+    '0' {
+        InfoText
+    }
+    '1' {
+        Custom
+    }
 
 }
-else { Custom}
+
+switch ($Default){
+    '0' {
+        InfoText
+    }
+    '1' {
+        Defaultdeploy
+        MSOffice
+        PDF
+    }
+
+}
 
 $cpuInfo = Get-CimInstance -ClassName Win32_Processor
 
