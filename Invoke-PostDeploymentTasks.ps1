6c7598b04d78b6609cbd271b36de4cee92b6bc5a
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-06-29 19:48:32 -0500

    create file

---
+++ Invoke-PostDeploymentTasks.ps1
@@ -0,0 +1,24 @@
+function Invoke-PostDeploymentTasks {
+<#
+    .SYNOPSIS 
+        This script automates the Post Deployment process.
+    .DESCRIPTION
+        This script aims to streamline and automate post-installation tasks by invoking several configurations and installations required for standard deployments.
+
+    param {
+
+    }
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+
+}
