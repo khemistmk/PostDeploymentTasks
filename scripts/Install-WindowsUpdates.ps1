629579708cbc7bfd6807afeabe84e2a0f7655ddd
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-06-30 19:24:45 -0500

    Update Install-WindowsUpdates.ps1

--- scripts/Install-WindowsUpdates.ps1
+++ scripts/Install-WindowsUpdates.ps1
@@ -1,7 +1,27 @@
 function Install-WindowsUpdates {
+<#
+    .SYNOPSIS 
+        This script automates the Post Deployment process.
+    .DESCRIPTION
+        This script aims to streamline and automate post-installation tasks by invoking several configurations and installations required for standard deployments.
+
+#>
+    [CmdletBinding()]
+    param ()
+    
+    begin {
+    }
+    
+    process {
     Write-Host "[*] Checking for Windows Updates..." -ForegroundColor Yellow
     Start-Process -Filepath "UsoClient.exe" -ArgumentList "ScanInstallWait" -Wait
     Write-Host "[*] Installing Windows Updates..." -ForegroundColor Yellow
     Start-Process -Filepath "UsoClient.exe" -ArgumentList "StartInstall" -Wait
-    Write-Host "[*] Reboot to complete Windows Updates..." -ForegroundColor Red   
+    Write-Host "[*] Reboot to complete Windows Updates..." -ForegroundColor Red  
+$WshShell = New-Object -comObject WScript. Shell $WshShell.RegWrite('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Orchestrator\InstallAtShutdown', '1', 'REG_DWORD')
+
+Restart-Computer 
+    }
+    end {
+    }
 }
