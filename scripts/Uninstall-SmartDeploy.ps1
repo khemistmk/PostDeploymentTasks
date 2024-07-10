87199d9f8612668979899a1c9c1665d6f74e01d6
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-10 18:48:57 -0500

    Update Uninstall-SmartDeploy.ps1

--- scripts/Uninstall-SmartDeploy.ps1
+++ scripts/Uninstall-SmartDeploy.ps1
@@ -1,5 +1,23 @@
-Function Uninstall-SmartDeploy {
-    $installed = Get-WmiObjecdt -Class Win32_Product | Where-Object {$_.Name -like '*SmartDeploy*'}
-    $installed.Uninstall()
-    Write-Host "[*] Smart Deploy Uninstalled" -ForegroundColor Green
+Function Uninstall-Package {
+        <#
+        .SYNOPSIS 
+            This script will set the default apps for new users on a machine.
+        .DESCRIPTION
+            This script sets Adobe, Foxit, or Chrome as the default .pdf viewer, Chrome as default browser, and Outlook as the default mail-to application.
+    #>
+        [CmdletBinding()]
+        param (
+            [Parameter(mandatory = $true)]
+            $PackageName
+        )
+
+        begin {
+
+        }
+
+        process {
+            $installed = Get-WmiObjecdt -Class Win32_Product |
+            Where-Object {$_.Name -like '*$PackageName*'}
+            $installed.Uninstall()
+            Write-Host "[*] $PackageName Uninstalled" -ForegroundColor Green
 }
