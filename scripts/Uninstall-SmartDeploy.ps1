448a33609cff5ac263e2f75ba5464d859e5e2392
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-10 18:51:09 -0500

    Update Uninstall-SmartDeploy.ps1

--- scripts/Uninstall-SmartDeploy.ps1
+++ scripts/Uninstall-SmartDeploy.ps1
@@ -1,9 +1,11 @@
 Function Uninstall-Package {
         <#
         .SYNOPSIS 
-            This script will set the default apps for new users on a machine.
+            This script will uninstall the named package.
         .DESCRIPTION
-            This script sets Adobe, Foxit, or Chrome as the default .pdf viewer, Chrome as default browser, and Outlook as the default mail-to application.
+            This script will uninstall the named package.
+        .PARAMETER PackageName
+            The name of the package to be uninstalled.
     #>
         [CmdletBinding()]
         param (
