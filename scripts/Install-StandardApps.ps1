6c2e73db13143b8743e6f5b401c1694c196ae908
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-10 12:55:19 -0500

    Update Install-StandardApps.ps1

--- scripts/Install-StandardApps.ps1
+++ scripts/Install-StandardApps.ps1
@@ -8,36 +8,39 @@ Function Install-StandardApps {
     [CmdletBinding()]
     param (
         [Parameter()]
-        
-
-        [Parameter()]
+        [string]$TemporaryDownloadFolder = "C:\Users\" + $($env:username) + "\AppData\Local\Temp\7Zip\",
         
     )
 
     begin {   
         Install-Module -Name Microsoft.WinGet.Client
+        $7zipWebsite = 'https://7-zip.org/'
+        $TemporaryDownloadFile = $TemporaryDownloadFolder + "7zip.exe" 
     }
 
     process {
+        Write-Host "[*] Installing Google Chrome..." - Foregroundcolor Yellow
         winget install -e --id Google.Chrome
+        Write-Host "[*] Google Chrome installed." - Foregroundcolor Green
+        Write-Host "[*] Installing Firefox..." - Foregroundcolor Yellow
         winget install -e --id Mozilla.Firefox
+        Write-Host "[*] Firefox installed." - Foregroundcolor Green
+        Write-Host "[*] Installing VLC..." - Foregroundcolor Yellow
         winget install -e --id VideoLAN.VLC
+        Write-Host "[*] VLC installed." - Foregroundcolor Green
+        Write-Host "[*] Installing Adobe Reader..." - Foregroundcolor Yellow
         winget install -e --id Adobe.Acrobat.Reader.64-bit
-        winget install -e --id Microsoft.Office
-
-        $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' |
-        Select-Object -ExpandProperty Links |
-        Where-Object {($_.outerHTML -match 'Download')-and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} |
-        Select-Object -First 1 |
-        Select-Object -ExpandProperty href)
-        # modified to work without IE
-        # above code from: https://perplexity.nl/windows-powershell/installing-or-updating-7-zip-using-powershell/
-        $installerPath = Join-Path.$env:TEMP (Split-Path $dlurl -Leaf)
-        Invoke-WebRequest $dlurl -OutFile $installerPath
-        Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
+        Write-Host "[*] Adobe Reader installed." - Foregroundcolor Green
+        Write-Host "[*] Installing 7zip..." - Foregroundcolor Yellow
+        $webLocation = $7zipWebsite + (Invoke-WebRequest -Uri $7zipWebsite | 
+            Select-Object -ExpandProperty Links | 
+            Where-Object {($_.innerHTML -eq 'Download') -and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} |
+            Select-Object -ExpandProperty href).Split(' ')[0]
+        Invoke-WebRequest $webLocation -OutFile $TemporaryDownloadFile
+        Start-Process $TemporaryDownloadFile -ArgumentList "/S" -Wait
     
     }
     end {
-    Remove-Item $installerPath
+    Remove-Item $TemporaryDownloadFolder -Recurse -Force
     }
 }
