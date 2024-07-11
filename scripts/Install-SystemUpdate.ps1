24a4926411781b3956c6042393e4a69327cdca4e
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-10 19:02:44 -0500

    Update Install-SystemUpdate.ps1

--- scripts/Install-SystemUpdate.ps1
+++ scripts/Install-SystemUpdate.ps1
@@ -1,4 +1,11 @@
 Function Install-SystemUpdate {
+        <#
+    .SYNOPSIS 
+        This script installs the OEM recommended system updater program and runs updates.
+    .DESCRIPTION
+        This script installs the OEM recommended system updater program and runs updates. supported OEMs are HP, Lenovo, and Dell.
+    #>
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]
         $setupfolder,
@@ -7,27 +14,35 @@ Function Install-SystemUpdate {
         $systemupdate = "System Update.exe",
 
         [Parameter()]
-        $HPIA = "HPIA.exe"
+        $HPIA = "HPIA.exe",
+        
+        [Parameter()]
+        $DellCommand = "DellCommand.exe"
     )
-    Write-Host "[*] Checking manufacturer..." -ForegroundColor Yellow
-    $manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer 
-    if ($Manufacturer -contains "Lenovo"){
-        Write-Host "[*] Installing Lenovo System Update..." -ForegroundColor Yellow
-        Start-Process -FilePath "$setupfolder\$systemupdate" -ArgumentList "/VERYSILENT /NORESTART" -Wait
-        $RegKey = "HKLM:\SOFTWARE\Policies\Lenovo\System Update\UserSettings\General"
-        $RegName = "AdminCommandLine"
-        $RegValue = "/CM -search A -action INSTALL -includerebootpackages 3 -noicon -noreboot -exporttowmi"    
-        # Create Subkeys if they don't exist
-        if (!(Test-Path $RegKey)) {
-        New-Item -Path $RegKey -Force | Out-Null
-        New-ItemProperty -Path $RegKey -Name $RegName -Value $RegValue | Out-Null
+    begin {
+        $manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
         }
-        else {
-        New-ItemProperty -Path $RegKey -Name $RegName -Value $RegValue -Force | Out-Null
-        } 
-        Start-Process -FilePath "C:\Program Files (x86)\Lenovo\System Update\Tvsu.exe" -ArgumentList "/CM"
-    }
-    elseif ($manufacturer -contains "HP"){
+
+    process {
+        Write-Host "[*] Checking manufacturer..." -ForegroundColor Yellow
+        switch ($manufacturer){
+            if ($Manufacturer -contains "Lenovo"){
+            Write-Host "[*] Installing Lenovo System Update..." -ForegroundColor Yellow
+            Start-Process -FilePath "$setupfolder\$systemupdate" -ArgumentList "/VERYSILENT /NORESTART" -Wait
+            $RegKey = "HKLM:\SOFTWARE\Policies\Lenovo\System Update\UserSettings\General"
+            $RegName = "AdminCommandLine"
+            $RegValue = "/CM -search A -action INSTALL -includerebootpackages 3 -noicon -noreboot -exporttowmi"    
+            # Create Subkeys if they don't exist
+            if (!(Test-Path $RegKey)) {
+                New-Item -Path $RegKey -Force | Out-Null
+                New-ItemProperty -Path $RegKey -Name $RegName -Value $RegValue | Out-Null
+            }
+            else {
+                New-ItemProperty -Path $RegKey -Name $RegName -Value $RegValue -Force | Out-Null
+            } 
+            Start-Process -FilePath "C:\Program Files (x86)\Lenovo\System Update\Tvsu.exe" -ArgumentList "/CM"
+        }
+        elseif ($manufacturer -contains "HP"){
         Start-Process -Filepath "$setupfolder\$HPIA" -ArgumentList "/s"
         Set-Location -FilePath "C:\SWSetup\SP140024"
         Start-Process -FilePath "HPImageAssistant.exe" -ArgumentList "/Action:Install /AutoCleanup /Category:BIOS, Drivers,Firmware /Silent"
