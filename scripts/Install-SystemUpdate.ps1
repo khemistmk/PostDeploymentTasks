414519ab3974f967bf73c9363f018ec61325f884
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-12 12:40:54 -0500

    Update Install-SystemUpdate.ps1

--- scripts/Install-SystemUpdate.ps1
+++ scripts/Install-SystemUpdate.ps1
@@ -26,6 +26,38 @@ Function Install-SystemUpdate {
     process {
         Write-Host "[*] Checking manufacturer..." -ForegroundColor Yellow
             if ($Manufacturer -contains "Lenovo"){
+              function global:au_GetLatest {
+                  $response = Invoke-RestMethod -Uri "https://download.lenovo.com/ibmdl/pub/pc/pccbbs/agent/SSClientCommon/HelloLevel_9_59_00.xml"
+                  if ($response.Length -gt 0) {
+        $xml = [xml] $response.Substring(3)
+        $version = $xml.LevelDescriptor.Version
+        $buildDate = $xml.LevelDescriptor.BuildDate
+    }
+    
+    $Latest = createLatest $version $buildDate
+
+    # Sometimes the version might be wrong, so check first
+    try {
+        Get-redirectedUri $Latest.URL32
+    }
+    catch {
+        # Fallback to scraping the webpage
+        $response = Invoke-WebRequest -Uri "https://support.lenovo.com/us/en/downloads/ds012808-lenovo-system-update-for-windows-10-7-32-bit-64-bit-desktop-notebook-workstation" -UseBasicParsing
+        $pattern = '(?<="Name":"Lenovo\ System\ Update")[\S\s]*,"Version":"(?<Version>[\d\.]+)'
+        $version = [regex]::Match($response.Content, $pattern).groups['Version'].value
+        $pattern = '(?<="Name":"Lenovo\ System\ Update")[\S\s]*,"Date":\{"Unix":(?<Date>\d+)\}'
+        $unixTime = [regex]::Match($response.Content, $pattern).groups['Date'].value
+        $buildDate = [System.DateTimeOffset]::FromUnixTimeMilliseconds($unixTime).DateTime.ToString("d MMM yyyy")
+        
+        $Latest = createLatest $version $buildDate
+    }
+
+    return $Latest
+}
+
+
+
+
             Write-Host "[*] Installing Lenovo System Update..." -ForegroundColor Yellow
             Start-Process -FilePath "$setupfolder\$systemupdate" -ArgumentList "/VERYSILENT /NORESTART" -Wait
             $RegKey = "HKLM:\SOFTWARE\Policies\Lenovo\System Update\UserSettings\General"
