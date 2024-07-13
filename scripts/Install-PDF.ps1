4132355fe885e30a01197864f22d6aeab22953f9
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-13 07:05:05 -0500

    Update Install-PDF.ps1

--- scripts/Install-PDF.ps1
+++ scripts/Install-PDF.ps1
@@ -32,6 +32,7 @@ Function Install-PDF {
             catch {
                 Write-Error -message "[*] Unable to install $InstallPDFVers"
             }
+        }
         if ($InstallPDFVers -eq "AdobeReader"){
             try {
                 Write-Host "[*] Installing $InstallPDFVers..." -ForegroundColor Yellow
@@ -40,6 +41,7 @@ Function Install-PDF {
             catch {
                 Write-Error -message "[*] Unable to install $InstallPDFVers"
             }
+        }
         if ($InstallPDFVers -eq "FoxitBusiness") {
             try {
                 Write-Host "[*] Installing $$InstallPDFVers..." -ForegroundColor Yellow
