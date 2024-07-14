4d1ca00516a4085a0987f99666c7eee29eab44ab
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-14 02:25:43 -0500

    Update Remove-Bloat.ps1

--- scripts/Remove-Bloat.ps1
+++ scripts/Remove-Bloat.ps1
@@ -28,6 +28,10 @@ Function Remove-Bloat {
 
             # Remove provisioned app from OS image, so the app won't be installed for any new users
             Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -like $app } | ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $_.PackageName }
+            New-Item -ItemType Directory -Force -Path C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState | Out-Null
+
+            #Copies start.bin  
+Copy-Item -path "$((Get-Item $PSScriptRoot).Parent.FullName)\Assets\\start.bin" -Destination "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\"`
         }
     }
     end{
