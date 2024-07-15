be3431923610ba857eb417fb2a5e19855aa72f95
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-14 22:24:16 -0500

    Update scripts

---
+++ scripts/Get-SystemStatus.ps1
@@ -0,0 +1,50 @@
+function Get-SystemStatus {
+    <#
+    .SYNOPSIS 
+        This script will disable the default Administrator account.
+    .DESCRIPTION
+        This script will check if the default Administrator account is enabled and if so, disable it.
+
+#>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {   
+        $adminaccount = Get-LocalUser -Name "Administrator"
+    }
+
+    process {
+        
+
+
+        $Report = @"
+
+Customer Name: $CustomerName ($Username)
+SN: $SerialNumber
+
+Activation Information:
+$MSOfficeVoucher
+$MSOfficeActivationEmail
+
+Computer Information:
+Manufacturer:         $manufacturer
+Model:                $model
+CPU:                  $CPUInfo
+RAM:                  $RAM
+Drive:                $Drive $Drivemanufacturer
+
+Dotnet 3.5:           $dotnet
+Power Options set:    $power
+
+Installed Software:
+$Software
+"@
+
+        }
+    }
+    end {
+
+    }
+}
