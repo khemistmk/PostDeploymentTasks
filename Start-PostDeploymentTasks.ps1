0d0ab1438f927407dd973d83bf25a1eca00a06a8
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-14 19:35:54 -0500

    Update Start-PostDeploymentTasks.ps1

--- Start-PostDeploymentTasks.ps1
+++ Start-PostDeploymentTasks.ps1
@@ -1,3 +1,4 @@
-Set-ExecutionPolicy Bypass
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 . .\Invoke-PostDeploymentTasks.ps1
 Invoke-PostDeploymentTasks -InstallOfficeVers O365 InstallPDFVers Adobe2020STD
