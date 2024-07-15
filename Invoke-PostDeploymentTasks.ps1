cd0813608893c2ef7aa83b8dd6a99b176d70b956
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-15 17:11:55 -0500

    changes

--- Invoke-PostDeploymentTasks.ps1
+++ Invoke-PostDeploymentTasks.ps1
@@ -9,34 +9,21 @@ function Invoke-PostDeploymentTasks {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [ValidateSet("O365","O365-32","OHBE21","OHBE21-32","OHBE19","OHBE19-32","None")]
+        [ValidateSet("O365","O365-32","OHBE21","OHBE21-32","OHBE19","OHBE19-32")]
         [string]$InstallOfficeVers,
         
         [Parameter()] 
-        [Validateset("AdobeReader","Adobe2020STD","Adobe2020Pro","FoxitBusiness","None")]
+        [Validateset("AdobeReader","Adobe2020STD","Adobe2020Pro","FoxitBusiness")]
         [string]$InstallPDFVers
     )
 
     begin {
         Write-Host "[*] Installing Prerequisites..." -ForegroundColor Yellow
         Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-        $nuget = Get-PackageProvider | Where-Object {$_.Name -eq "NuGet"}
-        if ($nuget){
-            return
-        }
-        else {
-            Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force
-        }
-        $winget = winget --version
-        if ($winget -like "*v1.8.*"){
-            return
-        }
-        else {
-            Install-Module -Name Microsoft.WinGet.Client
-        }
+        Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force
+        Install-Module -Name Microsoft.WinGet.Client
         Install-Module -Name PSWINDOWSUPDATE
         Import-Module $PSScriptRoot\PostDeploymentTasks.psm1
-
     }
 
     process {
@@ -54,11 +41,11 @@ function Invoke-PostDeploymentTasks {
             Set-BitlockerDrive C:
         }
         Invoke-Defaultdeploy
-        if ($InstallOfficeVers -eq "None") {Return}
-        if ($InstallOfficevers -Like "*O*") {
+        
+        if ($InstallOfficevers) {
         Install-MSOffice -officevers $InstallOfficevers
         }
-        if ($InstallPDFVers -eq "None") {Return}
+        if ($InstallPDFVers) {Return}
         if ($InstallPDFvers -Like "*A*" -or $InstallPDFVers -like "*F*") {
         Install-PDF -InstallPDFVers $InstallPDFVers
         }
