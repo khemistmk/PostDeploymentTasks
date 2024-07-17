20d46bdf7f49474bbf70e24af341709db485d88d
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-16 19:39:04 -0500

    Update Start-PostDeploymentTasks.ps1

--- Start-PostDeploymentTasks.ps1
+++ Start-PostDeploymentTasks.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass
 New-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp" -type Directory
-Invoke-WebRequest -Uri https://github.com/khemistmk/PostDeploymentTasks/archive/refs/heads/main.zip -OutFile "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip"
+Invoke-WebRequest -Uri "https://github.com/khemistmk/PostDeploymentTasks/archive/refs/heads/main.zip" -OutFile "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip"
 Expand-Archive -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip" -DestinationPath "$env:Userprofile\Documents\WindowsPowershell\temp\"
 Move-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks*" -Destination "$env:Userprofile\Documents\WindowsPowershell\Modules\PostDeploymentTasks"
 Remove-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\" -Recurse
