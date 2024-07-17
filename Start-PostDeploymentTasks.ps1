bf8e01377891629b06db28e52ea8e6b7029733b8
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-16 19:22:23 -0500

    Update Start-PostDeploymentTasks.ps1

--- Start-PostDeploymentTasks.ps1
+++ Start-PostDeploymentTasks.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass
 Invoke-WebRequest -Uri https://github.com/khemistmk/PostDeploymentTasks/archive/refs/heads/main.zip -OutFile "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip"
 Expand-Archive -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip" -DestinationPath "$env:Userprofile\Documents\WindowsPowershell\temp\"
-Remove-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip"
-Move-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks*" -Destination "$env:Userprofile\Documents\WindowsPowershell\PostDeploymentTask"
+Move-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks*" -Destination "$env:Userprofile\Documents\WindowsPowershell\Modules\PostDeploymentTasks"
+Remove-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\" -Recurse
 Import-Module PostDeploymentTasks
 Invoke-PostDeploymentTasks -InstallOfficeVers O365 -InstallPDFVers Adobe2020
