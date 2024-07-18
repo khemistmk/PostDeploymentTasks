6ec952f03af3242d8debe4b3c9588793bbb90d5a
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-18 07:59:33 -0500

    changees

--- Start-PostDeploymentTasks.ps1
+++ Start-PostDeploymentTasks.ps1
@@ -5,6 +5,11 @@ $params = @{
 
 Set-ExecutionPolicy Bypass
 New-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp" -type Directory
+$Moduledir = "$env:Userprofile\Documents\WindowsPowershell\Modules"
+if($Moduledir){$true}
+else {
+    New-Item -Path "$env:Userprofile\Documents\WindowsPowershell\Modules" -type Directory
+}
 Invoke-WebRequest -Uri "https://github.com/khemistmk/PostDeploymentTasks/archive/refs/heads/main.zip" -OutFile "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip"
 Expand-Archive -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks.zip" -DestinationPath "$env:Userprofile\Documents\WindowsPowershell\temp\"
 Move-Item -Path "$env:Userprofile\Documents\WindowsPowershell\temp\PostDeploymentTasks*" -Destination "$env:Userprofile\Documents\WindowsPowershell\Modules\PostDeploymentTasks"
